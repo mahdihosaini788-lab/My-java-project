d644e1c0b14be1123e914acc63eb58a268a785f0
mahdihosaini788-lab <mahdihosaini788@gmail.com>
2025-11-11 21:43:48 +0800

    Add files via upload

---
+++ Lottery.java
@@ -0,0 +1,35 @@
+
+package lottery;
+
+import java.util.Scanner;
+
+public class Lottery {
+
+    
+    public static void main(String[] args) {
+        
+        int lottery = (int)(Math.random()*100);
+        
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter your lottery pick (two digits)");
+        int guess = input.nextInt();
+        
+        int lotteryDigit1 = lottery/10;
+        int lotteryDigit2 = lottery%10;
+
+        int guessDigit1 = guess/10;
+        int guessDigit2 = guess%10;
+
+        System.out.println("The lottery number is: " + lottery);
+        
+        if(guess==lottery)
+            System.out.println("Exact match: you win 10_000$");
+        else if(guessDigit1==lotteryDigit2 || guessDigit2 == lotteryDigit1)
+            System.out.println("Match all digits: You win 3_000$");
+        else if(guessDigit1==lotteryDigit1||guessDigit1==lotteryDigit2||guessDigit2==lotteryDigit1||guessDigit2==lotteryDigit2)
+            System.out.println("Math one digit: You win 1_000$");
+        else System.out.println("Sorry, no match");
+        
+    }
+    
+}
