2db31b7619ff770d002d3da0397caf4b595741c8
mahdihosaini788-lab <mahdihosaini788@gmail.com>
2025-11-11 21:52:17 +0800

    Add files via upload

---
+++ PoundsToKilograms.java
@@ -0,0 +1,41 @@
+
+package poundstokilograms;
+
+import java.util.Scanner;
+
+public class PoundsToKilograms {
+    public static void main(String[] args) {
+        
+        Scanner input = new Scanner(System.in);
+        
+        System.out.println("Enter a number in pounds");
+        double pounds = input.nextDouble();
+        
+        double kilograms = pounds*0.454;
+        
+        System.out.println(pounds+"pounds is equals = "+kilograms+"kilograms");
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+    
+}
