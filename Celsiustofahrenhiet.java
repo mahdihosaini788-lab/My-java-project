558e1d3cb9dab1694e8db20fea93b28314ab1b3a
mahdihosaini788-lab <mahdihosaini788@gmail.com>
2025-11-11 20:31:39 +0800

    Add files via upload

---
+++ Celsiustofahrenhiet.java
@@ -0,0 +1,33 @@
+package celsiustofahrenhiet;
+
+import java.util.Scanner;
+
+public class Celsiustofahrenhiet {
+
+    public static void main(String[] args) {
+        
+        Scanner input = new Scanner(System.in);
+        
+        System.out.println("Enter degree in celsius: ");
+        double celsius = input.nextDouble();
+        
+        double fahrenhiet = (celsius*9/5)+32;
+        
+        System.out.println(fahrenhiet+"degree");
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+    
+}
