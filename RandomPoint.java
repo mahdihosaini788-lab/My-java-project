c13f191a63df6a993768d8988a5edee4c2289878
mahdihosaini788-lab <mahdihosaini788@gmail.com>
2025-11-11 21:54:30 +0800

    Add files via upload

---
+++ RandomPoint.java
@@ -0,0 +1,16 @@
+package randompoint;
+public class RandomPoint {
+
+    public static void main(String[] args) {
+        
+        
+               
+        int x  =(int)(Math.random()*100)-50;
+        int y  =(int)(Math.random()*200)-100;
+
+        System.out.println("The random point is ("+x+","+y+")");
+        
+        
+    }
+    
+}
