a92a9b90ffaa42154cdcf3a17349dc182f8f5064
mahdihosaini788-lab <mahdihosaini788@gmail.com>
2025-11-11 21:37:26 +0800

    Add files via upload

---
+++ DisplayingTime.java
@@ -0,0 +1,33 @@
+o
+package displayingtime;
+
+import java.util.Scanner;
+
+public class DisplayingTime {
+
+    public static void main(String[] args) {
+        
+                Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter an integer for seconds ");
+        int seconds = input.nextInt();        
+        
+       int minutes = seconds/60;
+       int remainingSeconds = seconds%60;
+       
+        System.out.println(seconds+"seconds is "+minutes+"minutes and "+remainingSeconds+"seconds ");
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+        
+
+    
+    
+}
