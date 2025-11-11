c6dc4655be5cec4fa17222f092c1ce7b8a4bbe1b
mahdihosaini788-lab <mahdihosaini788@gmail.com>
2025-11-11 21:56:31 +0800

    Add files via upload

---
+++ SimpleOfDemo.java
@@ -0,0 +1,32 @@
+package simpleofdemo;
+
+import java.util.Scanner;
+
+public class SimpleOfDemo {
+
+    
+    public static void main(String[] args) {
+        
+        Scanner input = new Scanner(System.in);
+        
+        System.out.println("Enter a number");
+        int num = input.nextInt();
+        
+        if(num%5==0)
+            System.out.println("Hi five");
+        else if(num%2==0)
+            System.out.println("Hi even");
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+    
+}
