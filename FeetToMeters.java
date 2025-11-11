dffba03d873da4bf5be53bf43ccd2617a536bee3
mahdihosaini788-lab <mahdihosaini788@gmail.com>
2025-11-11 21:38:33 +0800

    Add files via upload

---
+++ FeetToMeters.java
@@ -0,0 +1,34 @@
+package feettometers;
+
+import java.util.Scanner;
+
+
+public class FeetToMeters {
+
+    
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        
+        System.out.println("Enter number in feet :");
+         double feet = input.nextDouble();
+         
+         double meters = feet*0.305;
+        
+        System.out.println(feet+"feet is equals = "+meters+"meters");
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+    
+}
