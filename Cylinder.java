ee2467976cbfe33da16f4e3f201a2a48f931e0e6
mahdihosaini788-lab <mahdihosaini788@gmail.com>
2025-11-11 21:36:23 +0800

    Add files via upload

---
+++ Cylinder.java
@@ -0,0 +1,46 @@
+package cylinder;
+
+import java.util.Scanner;
+
+public class Cylinder {
+
+    
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        System.out.println("Enter a number for radius :");
+        double radius = input.nextDouble();
+        
+        System.out.println("Enter a number for height");
+        double height = input.nextDouble();
+        
+        final double PI = 3.14;
+        
+        
+        
+        double cylinder = PI*radius*radius*height;
+        
+        System.out.println(cylinder);
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+    
+}
