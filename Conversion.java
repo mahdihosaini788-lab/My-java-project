2470637b14530ea5d293eb3b15ef0b1359a06ef1
mahdihosaini788-lab <mahdihosaini788@gmail.com>
2025-11-11 21:33:59 +0800

    Add files via upload

---
+++ Conversion.java
@@ -0,0 +1,94 @@
+
+package conversion;
+
+
+public class Conversion {
+
+    
+    public static void main(String[] args) {
+        
+
+/*   int a =100, b = 3;
+int c = a/b;     // = 33
+*/
+ /*int a =100, b = 3;
+double c =(double) a/b;     // = 33.33333
+
+
+ int a =100, b = 3;
+int c = (int)((double)a/b);     // = 33
+
+String str = "101";
+int a = Integer.parseInt(str,16);
+
+        System.out.println(a);
+
+
+
+int b = 20;
+        System.out.println(String.valueOf(b));
+
+
+
+*/
+ String s = "true";
+ boolean b =Boolean.parseBoolean(s);
+
+
+        System.out.println(b);
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+
+
+
+        // byte > short > int > long > float > double
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+    
+}
